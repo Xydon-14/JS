93826710cb44c60b6c0446e122b3ff2eb85e6d05
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-10 20:12:46 +0000

    basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144522;
+let accountEmail = "lokesh@gmail.com"
+var accountPassword = "1234"
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 34 Not Allowed 
+
+/*
+dont't use var because it has scope resolution problems
+*/ 
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treats all js codes as newer version
+
+//alert(4+4) we are using node js not browser
+
+console.log(4
+    +
+    3) // code readability
+
+    // documentation tc39 ; mdn
+console.log("LOKESH")
