0ba1b5c33d42aa68e29c18439c597d2699ed66a8
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-11 19:58:19 +0000

    strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,36 @@
+// let name = "Lokesh"
+// let repoCount = 50
+// //console.log(name + repoCount + " value"); old way
+
+// //modern way
+// console.log(`My name is ${name} and My repository count is ${repoCount}.`)
+
+// //second way to declare a string 
+const gameName = new String("LokeshJ-hs-com")
+// console.log(gameName[5]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('o'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-9,9)
+console.log(anotherString);
+
+const string1 = "            lokesh      "
+console.log(string1.trim())
+
+const url = "https://lokesh.com/lokesh$20$jaiswar"
+console.log(url.replace("$20$","-"))
+console.log(url.includes("lokesh"));
+console.log(gameName.split("-")); // returns array
+
+
+
+
+
+
