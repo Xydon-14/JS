bdbe820007e76d8aaad4038999737fef78ffe30a
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-11 08:51:59 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //return Nan (not a number)
+
+
+let name = 1405
+let valueInString = String(name)
+console.log(typeof valueInString)
+console.log(valueInString);
