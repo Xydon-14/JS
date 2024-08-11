a934d734cf3747301dd13d131dba6b79b89b02af
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-11 08:29:58 +0000

    datatype

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -7,4 +7,25 @@ console.log(4
     3) // code readability
 
     // documentation tc39 ; mdn
+
 console.log("LOKESH")
+
+let name = "Lokesh" //string
+let age = 19 //number 
+let isLoggedIn = false //boolean
+let state; //undefined
+let value = null 
+
+//number => 2 to power 53
+//bigint
+//string => "" 2 raise to 53-1
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "Lokesh");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
