eada3d95041cf75ae1b8a5e49a91a3061b52254e
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-11 17:17:02 +0000

    comparison

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -9,15 +9,16 @@
 // console.log("02" > 1);
 
 //js treats equality(==) and comaprison operators differently during comparison operators it converts null to 0
-console.log(null > 0);    
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);    
+// console.log(null == 0);
+// console.log(null >= 0);
 
-//js is not predictable because it has no set of rules therefore there is typescript which has many rules
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// //js is not predictable because it has no set of rules therefore there is typescript which has many rules
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
-// === 
+// // === strict check
+// console.log("2" === 2);
 
-console.log("2" === 2);
+
