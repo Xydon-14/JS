ea499ed4e1a4be367e2e9ae7be833321b4f897cd
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-12 15:33:11 +0000

    number

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,16 @@
+const score = 400;
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); //decimal ke baad kinta no chahiye
+
+const otherNumber = 231.8966
+console.log(otherNumber.toPrecision(3));
+//kitne value ka precision chahiye
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());
+
+
+
