602dd4adc28d3cbbaf6dad5c7015a99f8d466098
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-12 17:33:19 +0000

    maths

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -1,16 +1,34 @@
 const score = 400;
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2)); //decimal ke baad kinta no chahiye
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //decimal ke baad kinta no chahiye
 
 const otherNumber = 231.8966
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 //kitne value ka precision chahiye
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-IN"));
+//understand string and number methods
+
+// *****************Maths**************************
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.max(4.2,45,6,7));
+console.log(Math.min(4.2,45,6,7));
+
+console.log(Math.random());//by default 0 se 1 ke bich mai
+console.log((Math.random()*10)+1);
+
+
+
+
+
 
 
 
