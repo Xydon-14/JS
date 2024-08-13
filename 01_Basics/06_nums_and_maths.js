6a001eb92ac2c15f0cf4ac99e87e9769e9eb1fc3
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-13 14:37:47 +0000

    date

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -15,15 +15,22 @@ const hundreds = 1000000
 //understand string and number methods
 
 // *****************Maths**************************
-console.log(Math.abs(-4));
-console.log(Math.round(4.3));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.2));
-console.log(Math.max(4.2,45,6,7));
-console.log(Math.min(4.2,45,6,7));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.max(4.2,45,6,7));
+// console.log(Math.min(4.2,45,6,7));
 
-console.log(Math.random());//by default 0 se 1 ke bich mai
-console.log((Math.random()*10)+1);
+// console.log(Math.random());//by default 0 se 1 ke bich mai
+// console.log((Math.random()*10)+1); //(0.0)multiplying 10 to shift the vale to left side and adding one to get the minimum value 1
+// console.log(Math.round((Math.random()*10)+1)); 
+
+const max = 20;
+const min = 10;
+
+console.log(Math.round(Math.random()*(max-min+1)+min)) //sir's method 
+console.log(Math.round((Math.random()*10)+min))        // mine
 
 
 
---
+++ 01_Basics/07_datesInJS.js
@@ -0,0 +1,37 @@
+let myDate = new Date() //object
+console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+//to declare a date 
+// let myCreatedDate = new Date(2025,0o2,14,5,0,3);//jo month uska ek kam likhne ka (because array starts with 0)
+// let myCreatedDate = new Date("2025-03-14");yyyy-mm-dd//string mai exact likhne ka
+let myCreatedDate = new Date("03-14-2005");//mm-dd-yy//string mai exact likhne ka
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp); //value in millisecondds from 1-1-1970
+// console.log(myCreatedDate.getTime());//to get date in miliseconds
+
+// console.log(Date.now());//value is in milisecond
+// console.log(Math.round(Date.now()/1000))// to convert in seconds and math.round to avoid decimal values
+
+let newDate = new Date()
+console.log(newDate.getMonth()+1);//because index is returned
+console.log(newDate.getDate());
+
+// string interpolation = `${}`
+
+newDate.toLocaleString("default",{
+    weekday:"long"
+})
+
+
+
+
+
+
