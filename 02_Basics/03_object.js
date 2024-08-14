c384637b1ca11c88e97aa1ad105d19fcf895fe97
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-14 09:12:49 +0000

    object

---
+++ 02_Basics/03_object.js
@@ -0,0 +1,37 @@
+// singleton - when made using constructor
+// Object.create
+
+//object literals
+const mySym = Symbol("key1")
+const JsUser = {
+    name:"lokesh",//be default it will store "name"
+    "full name":"lokesh jaiswar",// this value can not be accessed through dot
+    [mySym]: "mykey1",//to use symbol krrp the key in brackets
+    age : 19,
+    location : "Mumbai",
+    email: "lokesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+// console.log(mySym);
+
+JsUser.email = "lokesh@chatgpt.com" //previous will be over rided
+// Object.freeze(JsUser);//now no changes can be done
+JsUser.email = "lokesh@microsoft.com" //previous will be over rided
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+} 
+
+console.log(JsUser.greeting);
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
