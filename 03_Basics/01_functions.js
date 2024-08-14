8e3471e1b7b898a20de5c052ab7b4956b306f82a
Xydon-14 <lokeshjaiswar140305@gmail.com>
2024-08-14 16:35:16 +0000

    funtions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+    console.log('L');
+    console.log('O');
+    console.log('K');
+    console.log('E');
+    console.log('S');
+    console.log('H');   
+}
+// console.log(sayMyName) //reference
+// sayMyName() //execution
+
+function addTwoNumbers(number1,number2){
+    console.log(number1+number2);    
+}
+addTwoNumbers(3,4);
